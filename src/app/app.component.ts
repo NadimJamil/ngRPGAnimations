054c7f7d7d55531267ea4f0733f49dd87789b98a
Nadim Jamil <2261561@cegepmontpetit.ca>
2024-10-29 17:23:38 -0400

    FCT animation lab

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import {transition, trigger, useAnimation} from "@angular/animations";
+import { shakeX } from 'ng-animate';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations : [
+    trigger('death', [transition(':increment', useAnimation(shakeX, {params: {timing: 0.5}}))])
+  ]
 })
 export class AppComponent {
   slimeIsPresent = false;
-
+  shakeX = 0.5
+  ng_death = false;
   constructor() {
   }
 
@@ -20,8 +25,10 @@ export class AppComponent {
   death(){
     this.slimeIsPresent = false;
     // TODO Animation angular avec forwards
-
+    this.ng_death = true;
+    setTimeout(() => {this.ng_death = false}, 1500);
     // TODO 2e animation angular en même temps
+    
   }
 
   attack(){
@@ -31,5 +38,17 @@ export class AppComponent {
 
   hit(){
     // TODO Utilisé Animista pour faire une animation différente avec css (wobble)
+  }
+
+  showSlime(){
+    var element = document.getElementById("slimeyId");
+    element?.classList.remove("fadeOut");
+    element?.classList.add("fadeIn");
+  }
+
+  hideSlime(){
+    var element = document.getElementById("slimeyId");
+    element?.classList.remove("fadeIn");
+    element?.classList.add("fadeOut");
   }
 }
